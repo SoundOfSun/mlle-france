240f5d76e964a56cce08d8dd5316a107860960de
Cecile Alfred <cecile.alfredx@gmail.com>
2017-09-14 16:58:22 +0100

    added title to index page and added type column to Projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913152253) do
+ActiveRecord::Schema.define(version: 20170914155639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170913152253) do
     t.string "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
   end
 
 end
