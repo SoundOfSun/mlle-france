0c2fc029b8faacb5821498f41fad0c4cfacfd51e
Cecile Alfred <cecile.alfredx@gmail.com>
2017-09-19 15:41:29 +0100

    Added link to Projects to generate the buttons on Projects page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170915152226) do
+ActiveRecord::Schema.define(version: 20170919144008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170915152226) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "job"
+    t.string "link"
   end
 
   add_foreign_key "photos", "projects"
